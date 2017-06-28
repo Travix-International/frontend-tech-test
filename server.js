c571a4f59318f98347fae8c7dd4d3f66cbe90041
leandrooriente <leandroriente@gmail.com>
2017-06-27 23:10:33 -0300

    Make task resource restfull

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 'use strict';
 
-const app = require('express')();
-const tasksContainer = require('./tasks.json');
+const express = require('express');
+const bodyParser = require('body-parser');
+const tasks = require('./tasks.json');
+
+const app = express();
+
+app.use(bodyParser());
 
 /**
  * GET /tasks
- * 
+ *
  * Return the list of tasks with status code 200.
  */
-app.get('/tasks', (req, res) => {
-  return res.status(200).json(tasksContainer);
+app.get('/task', (req, res) => {
+  return res.status(200).json(tasks);
 });
 
 /**
  * Get /task/:id
- * 
+ *
  * id: Number
- * 
+ *
  * Return the task for the given id.
- * 
+ *
  * If found return status code 200 and the resource.
  * If not found return status code 404.
  * If id is not valid number return status code 400.
@@ -27,75 +32,81 @@ app.get('/task/:id', (req, res) => {
   const id = parseInt(req.params.id, 10);
 
   if (!Number.isNaN(id)) {
-    const task = tasks.Container.find((item) => item.id === id);
+    const task = tasks.find(item => item.id === id);
 
     if (task !== null) {
       return res.status(200).json({
         task,
       });
-    } else {
-      return res.status(404).json({
-        message: 'Not found.',
-      });
     }
-  } else {
-    return res.status(400).json({
-      message: 'Bad request.',
+
+    return res.status(404).json({
+      message: 'Not found.',
     });
   }
+
+  return res.status(400).json({
+    message: 'Bad request.',
+  });
 });
 
 /**
  * PUT /task/update/:id/:title/:description
- * 
+ *
  * id: Number
  * title: string
  * description: string
- * 
+ *
  * Update the task with the given id.
  * If the task is found and update as well, return a status code 204.
  * If the task is not found, return a status code 404.
  * If the provided id is not a valid number return a status code 400.
  */
-app.put('/task/update/:id/:title/:description', (req, res) => {
+app.put('/task/:id', (req, res) => {
   const id = parseInt(req.params.id, 10);
 
   if (!Number.isNaN(id)) {
-    const task = tasksContainer.tasks.find(item => item.id === id);
+    const task = tasks.find(item => item.id === id);
 
     if (task !== null) {
-      task.title = req.params.title;
-      task.description = req.params.description;
+      task.title = req.body.title;
+      task.description = req.body.description;
       return res.status(204);
-    } else {
-      return res.status(404).json({
-        message: 'Not found',
-      });
     }
-  } else {
-    return res.status(400).json({
-      message: 'Bad request',
+
+    return res.status(404).json({
+      message: 'Not found',
     });
   }
+
+  return res.status(400).json({
+    message: 'Bad request',
+  });
 });
 
 /**
  * POST /task/create/:title/:description
- * 
+ *
  * title: string
  * description: string
- * 
+ *
  * Add a new task to the array tasksContainer.tasks with the given title and description.
  * Return status code 201.
  */
-app.post('/task/create/:title/:description', (req, res) => {
+app.post('/task', (req, res) => {
+  if (!req.body || !req.body.title) {
+    return res.status(400).json({
+      message: 'Bad request. Missing title parameter.'
+    });
+  }
+
   const task = {
-    id: tasksContainer.tasks.length,
-    title: req.params.title,
-    description: req.params.description,
+    id: tasks.length,
+    title: req.body.title,
+    description: req.body.description
   };
 
-  tasksContainer.tasks.push(task);
+  tasks.push(task);
 
   return res.status(201).json({
     message: 'Resource created',
@@ -112,20 +123,20 @@ app.post('/task/create/:title/:description', (req, res) => {
  * If the task is not found, return a status code 404.
  * If the provided id is not a valid number return a status code 400.
  */
-app.delete('/task/delete/:id', (req, res) => {
+app.delete('/task/:id', (req, res) => {
   const id = parseInt(req.params.id, 10);
 
   if (!Number.isNaN(id)) {
-    const task = tasksContainer.tasks.find(item => item.id === id);
-  
+    const task = tasks.find(item => item.id === id);
+
     if (task !== null) {
-      const taskIndex = tasksContainer.tasks;
-      tasksContainer.tasks.splice(taskIndex, 1);
+      const taskIndex = tasks;
+      tasks.splice(taskIndex, 1);
       return res.status(200).json({
         message: 'Updated successfully',
       });
     } else {
-      return es.status(404).json({
+      return res.status(404).json({
         message: 'Not found',
       });
     }
