bd8f8a69def09bf0f487e22cf1ba88ee96e98f78
leandrooriente <leandroriente@gmail.com>
2017-06-28 21:00:58 -0300

    Add debounced update to todo title

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import debounce from 'lodash/debounce';
+
 import style from './style.scss';
 
 const propTypes = {
@@ -9,32 +11,41 @@ const propTypes = {
   editTodo: PropTypes.func.isRequired
 };
 
-const List = ({ todos, deleteTodo, editTodo }) => (
-  <ul className={style.wrapper}>
-    { todos.map(l => (
-      <li className={style.item} key={l.id}>
-        <button
-          className={style.active}
-          onClick={() => editTodo({ ...l, completed: !l.completed }, l.id)}
-        >
-          { l.completed && (<span className={style.checkIcon}>&#x2713;</span>) }
-        </button>
+const List = ({ todos, deleteTodo, editTodo }) => {
+  const debounceUpdate = debounce(editTodo, 250);
 
-        <input
-          className={`${style.editInput} ${l.completed && style.completed}`}
-          defaultValue={l.title}
-        />
+  const handleTitleChange = (event, todo) => (
+    debounceUpdate({ ...todo, title: event.target.value }, todo.id)
+  );
 
-        <button
-          className={style.remove}
-          onClick={() => deleteTodo(l.id)}
-        >
-          &times;
-        </button>
-      </li>
-    ))}
-  </ul>
-);
+  return (
+    <ul className={style.wrapper}>
+      { todos.map(l => (
+        <li className={style.item} key={l.id}>
+          <button
+            className={style.active}
+            onClick={() => editTodo({ ...l, completed: !l.completed }, l.id)}
+          >
+            { l.completed && (<span className={style.checkIcon}>&#x2713;</span>) }
+          </button>
+
+          <input
+            className={`${style.editInput} ${l.completed && style.completed}`}
+            defaultValue={l.title}
+            onChange={e => handleTitleChange(e, l)}
+          />
+
+          <button
+            className={style.remove}
+            onClick={() => deleteTodo(l.id)}
+          >
+            &times;
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 List.propTypes = propTypes;
 
