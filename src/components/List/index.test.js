bf13b73e3b9abb6a0379bab5ef47e326341832d8
leandrooriente <leandroriente@gmail.com>
2017-06-29 23:07:19 -0300

    Add <List /> tests

---
+++ src/components/List/index.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Item from './Item';
+import Component from '.';
+
+describe('<List />', () => {
+  const deleteTodo = jest.fn();
+  const editTodo = jest.fn();
+  const todosList = [{
+    id: 92916525,
+    title: 'veniam id minim anim',
+    completed: true
+  },
+  {
+    id: 46769446,
+    title: 'velit ullamco amet enim',
+    completed: false
+  },
+  {
+    id: 59389998,
+    title: 'cupidatat incididunt velit ut',
+    completed: true
+  }];
+
+  const renderWrapper = (isFetching, isEmpty, todos) => shallow(
+    <Component
+      deleteTodo={deleteTodo}
+      editTodo={editTodo}
+      isEmpty={isEmpty}
+      isFetching={isFetching}
+      todos={todos}
+    />
+  );
+
+  it('should render a loading div', () => {
+    const wrapper = renderWrapper(true, false, []);
+    expect(wrapper.find('.loading')).toHaveLength(1);
+  });
+
+  it('should render an empty alert', () => {
+    const wrapper = renderWrapper(false, true, []);
+    expect(wrapper.find('.emptyList')).toHaveLength(1);
+  });
+
+  // React Virtualized Autosizer requires a real DOM to work
+});
