8d5c053f1654a37e6be243d1498ea6cc8670ff82
Gustavo Isensee <gustavo.isensee@gmail.com>
2017-07-04 19:14:23 -0300

    Added cors on api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import ejs from 'ejs';
 import path from 'path';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import routes from './src/routes/start';
 
 const app = express();
@@ -10,6 +11,9 @@ const port = process.env.PORT || 4000;
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+app.use(cors());
+app.set('trust_proxy', 1);
 
 app.set('views', path.join(__dirname, '/src/view'));
 app.set('view engine', 'ejs');
