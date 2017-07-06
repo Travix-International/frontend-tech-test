a6a85da03a199389775df5ffaae06e0070bd3c21
gui_gsantos <guilherme.goncalvessantos@gmail.com>
2017-07-06 00:05:02 -0300

    improved some styles

--- client/src/components/tasks_list_item.js
+++ client/src/components/tasks_list_item.js
@@ -12,11 +12,12 @@ class TasksListItem extends Component {
 
     return (
       <li className="taskslist-item">
-        <div className="btn-check" onClick={this.onDelete.bind(this)}>
+        <div className="btn-icon" onClick={this.onDelete.bind(this)}>
           <svg className="check-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"/>
           </svg>
         </div>
+
 
         <h2>{task.title}</h2>
         <div className="description">{task.description}</div>
