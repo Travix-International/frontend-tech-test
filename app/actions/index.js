c0065240792f9cd3a774138474af88c9f504a7c1
nmamikonyan <narek.mamikonyan@monitis.com>
2017-07-12 19:45:18 +0400

    working on async

--- app/actions/index.js
+++ app/actions/index.js
@@ -22,11 +22,42 @@ export const toggleTodo = id => {
         id
     }
 };
-
-
-function requestTasks(tasks) {
+export function fetchingFailed(bool) {
     return {
-        type: FETCH.GET_TASKS,
-        tasks
-    }
+        type: 'FETCHING_FAILED',
+        hasErrored: bool
+    };
+}
+
+export function isLoading(bool) {
+    return {
+        type: 'IS_LOADING',
+        isLoading: bool
+    };
+}
+
+export function fetchDataSuccess(items) {
+    return {
+        type: 'FETCH_DATA_SUCCESS',
+        items
+    };
+}
+export function fetchData(url) {
+    return (dispatch) => {
+        dispatch(isLoading(true));
+
+        fetch(url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+
+                dispatch(isLoading(false));
+
+                return response;
+            })
+            .then((response) => response.json())
+            .then((items) => dispatch(fetchDataSuccess(items)))
+            .catch(() => dispatch(fetchingFailed(true)));
+    };
 }
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -3,8 +3,40 @@ import { combineReducers } from 'redux';
 import todos from './todos'
 import visibilityFilter from './visibilityFilter'
 
+export function fetchingFailed(state = false, action) {
+    switch (action.type) {
+        case 'FETCHING_FAILED':
+            return action.hasErrored;
+
+        default:
+            return state;
+    }
+}
+
+export function isLoading(state = false, action) {
+    switch (action.type) {
+        case 'IS_LOADING':
+            return action.isLoading;
+
+        default:
+            return state;
+    }
+}
+
+export function fetchDataSuccess(state = [], action) {
+    switch (action.type) {
+        case 'FETCH_DATA_SUCCESS':
+            return action.items;
+
+        default:
+            return state;
+    }
+}
 const rootReducer = combineReducers({
     visibilityFilter,
+    fetchingFailed,
+    isLoading,
+    fetchDataSuccess,
     todos,
     routing
 });
