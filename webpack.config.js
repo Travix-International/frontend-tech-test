158561fec813c9489d51556dc89738989e2a8eec
Rolando Perez <me@rolilink.com>
2017-08-23 18:48:20 -0500

    Initial Setup: added webpack | eslint | development scripts

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/src/index.html',
+  filename: 'index.html',
+  inject: 'body',
+});
+
+module.exports = {
+  entry: './client/src/index.js',
+  output: {
+    path: path.resolve('./client/static'),
+    filename: 'todo.bundle.js',
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+    ],
+  },
+  devServer: {
+    proxy: {
+      '/api': 'http://localhost:9001',
+    },
+  },
+  devtool: 'cheap-module-eval-source-map',
+  plugins: [HtmlWebpackPluginConfig],
+};
