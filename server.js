ab374b9a8d3fc18e9f9ba88846fc48afd7bbcc71
Marcos Miani <marcos.miani@gmail.com>
2017-09-26 21:57:44 -0300

    Server small improvements added

--- server.js
+++ server.js
@@ -3,9 +3,12 @@
 const app = require('express')();
 const tasksContainer = require('./tasks.json');
 
+const getTasksObject = () => tasksContainer.tasks;
+const getTasksLastID = () => parseInt(tasksContainer.lastAdded, 10);
+
 /**
  * GET /tasks
- * 
+ *
  * Return the list of tasks with status code 200.
  */
 app.get('/tasks', (req, res) => {
@@ -14,11 +17,11 @@ app.get('/tasks', (req, res) => {
 
 /**
  * Get /task/:id
- * 
+ *
  * id: Number
- * 
+ *
  * Return the task for the given id.
- * 
+ *
  * If found return status code 200 and the resource.
  * If not found return status code 404.
  * If id is not valid number return status code 400.
@@ -27,7 +30,7 @@ app.get('/task/:id', (req, res) => {
   const id = parseInt(req.params.id, 10);
 
   if (!Number.isNaN(id)) {
-    const task = tasks.Container.find((item) => item.id === id);
+    const task = getTasksObject().find((item) => item.id === id);
 
     if (task !== null) {
       return res.status(200).json({
@@ -47,11 +50,11 @@ app.get('/task/:id', (req, res) => {
 
 /**
  * PUT /task/update/:id/:title/:description
- * 
+ *
  * id: Number
  * title: string
  * description: string
- * 
+ *
  * Update the task with the given id.
  * If the task is found and update as well, return a status code 204.
  * If the task is not found, return a status code 404.
@@ -61,7 +64,7 @@ app.put('/task/update/:id/:title/:description', (req, res) => {
   const id = parseInt(req.params.id, 10);
 
   if (!Number.isNaN(id)) {
-    const task = tasksContainer.tasks.find(item => item.id === id);
+    const task = getTasksObject().find(item => item.id === id);
 
     if (task !== null) {
       task.title = req.params.title;
@@ -81,21 +84,21 @@ app.put('/task/update/:id/:title/:description', (req, res) => {
 
 /**
  * POST /task/create/:title/:description
- * 
+ *
  * title: string
  * description: string
- * 
+ *
  * Add a new task to the array tasksContainer.tasks with the given title and description.
  * Return status code 201.
  */
 app.post('/task/create/:title/:description', (req, res) => {
   const task = {
-    id: tasksContainer.tasks.length,
+    id: getTasksLastID() + 1,
     title: req.params.title,
     description: req.params.description,
   };
 
-  tasksContainer.tasks.push(task);
+  getTasksObject().push(task);
 
   return res.status(201).json({
     message: 'Resource created',
@@ -104,9 +107,9 @@ app.post('/task/create/:title/:description', (req, res) => {
 
 /**
  * DELETE /task/delete/:id
- * 
+ *
  * id: Number
- * 
+ *
  * Delete the task linked to the  given id.
  * If the task is found and deleted as well, return a status code 204.
  * If the task is not found, return a status code 404.
@@ -116,11 +119,11 @@ app.delete('/task/delete/:id', (req, res) => {
   const id = parseInt(req.params.id, 10);
 
   if (!Number.isNaN(id)) {
-    const task = tasksContainer.tasks.find(item => item.id === id);
-  
-    if (task !== null) {
-      const taskIndex = tasksContainer.tasks;
-      tasksContainer.tasks.splice(taskIndex, 1);
+    const tasks = getTasksObject();
+    const taskIndex = tasks.findIndex(item => item.id === id);
+
+    if (taskIndex !== -1) {
+      tasks.splice(taskIndex, 1);
       return res.status(200).json({
         message: 'Updated successfully',
       });
@@ -134,6 +137,20 @@ app.delete('/task/delete/:id', (req, res) => {
       message: 'Bad request',
     });
   }
+});
+
+const path = require('path');
+const indexHTMLFilename = path.join(path.resolve(__dirname), 'src/index.html');
+const bundleFilename = path.join(path.resolve(__dirname), 'dist/bundle.js');
+
+/* Bundle entry point */
+app.get(/bundle\.js$/, (req, res) => {
+  res.sendFile(bundleFilename);
+});
+
+/* Application entry point */
+app.get('/', (req, res) => {
+  res.sendFile(indexHTMLFilename);
 });
 
 app.listen(9001, () => {
