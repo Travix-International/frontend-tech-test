04ed7fea46948214cadd3b86966a9557ff6cdd5f
Marcos Miani <marcos.miani@gmail.com>
2017-09-27 00:18:26 -0300

    Add task action and reducer

--- src/app.ducks.js
+++ src/app.ducks.js
@@ -1,29 +1,42 @@
 import { combineReducers } from 'redux';
 
 const initialState = {
-  todos: []
+  todos: new Map(),
+  lastTaskID: 0
 };
 
 const LOAD_TODOS = 'app/LOAD_TODOS';
-const loadAction = todos => ({
+const loadTasks = todos => ({
   type: LOAD_TODOS,
   todos
 });
 
+const ADD_TODO = 'app/ADD_TODO';
+const addTask = (title, desctiption) => ({
+  type: ADD_TODO,
+  title,
+  desctiption
+});
+
 const todos = (state = initialState.todos, action) => {
+  const { title, desctiption } = action;
+  const id = state.size + 1;
   switch (action.type) {
     case LOAD_TODOS:
-      return action.todos;
+      return new Map(action.todos.map((t, i) => [i, t]));
+    case ADD_TODO:
+      return new Map(state).set(id, { id, title, desctiption, isDone: false });
     default:
       return state;
   }
 };
 
 const reducer = combineReducers({
-  todos,
+  todos
 });
 
 export {
-  loadAction,
+  addTask,
+  loadTasks,
   reducer as default,
 };
