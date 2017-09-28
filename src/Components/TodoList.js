f045e62b98ba210f0628e0fc602c54c9281fca61
Marcos Miani <marcos.miani@gmail.com>
2017-09-28 00:08:52 -0300

    Improved list behavior on large sizes

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -3,22 +3,43 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { AutoSizer, List } from 'react-virtualized';
 import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import CloseIcon from 'material-ui-icons/Close';
+import EditIcon from 'material-ui-icons/Edit';
+import AddIcon from 'material-ui-icons/Add';
 import './TodoList.scss';
 
 const Todo = (props) => {
-  const { todo } = props;
-  let TodoComponent = <span>{todo.title}</span>;
+  const { task, index, style, selectTaskIndex, removeTask } = props;
 
-  if (todo.isDone) {
-    TodoComponent = <strike><Button>{todo.title}</Button></strike>;
-  }
-
-  return TodoComponent;
+  return (
+    <div
+      className="list-row"
+      style={style}
+    >
+      <div
+        className="list-row__wrapper"
+      >
+        <div className="list-row__id">{task.id}</div>
+        <div className="list-row__title">{task.title}</div>
+        <div className="list-row__description">{task.description}</div>
+        <IconButton className="list-row__button" onClick={() => selectTaskIndex(index)}>
+          <EditIcon />
+        </IconButton>
+        <IconButton className="list-row__button" onClick={() => removeTask(task.id, index)}>
+          <CloseIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
 };
 
 Todo.propTypes = {
-  dispatch: PropTypes.func,
-  todo: PropTypes.object
+  selectTaskIndex: PropTypes.func,
+  removeTask: PropTypes.func,
+  index: PropTypes.number,
+  style: PropTypes.object,
+  task: PropTypes.object
 };
 
 class TodoList extends Component {
@@ -27,23 +48,34 @@ class TodoList extends Component {
   }
 
   render() {
-    const { todos, removeTask } = this.props;
+    const { todos, removeTask, selectTaskIndex } = this.props;
     return (
       <div className="todo-list">
+        <Button
+          aria-label="add"
+          className="todo-list__add-button"
+          color="primary"
+          fab
+        >
+          <AddIcon />
+        </Button>
         <AutoSizer>
           {({ width, height }) => (
             <List
               className="todo-list__main"
               height={height}
               rowCount={todos.length}
-              rowHeight={30}
+              rowHeight={48}
               rowRenderer={({ index, key, style }) => {
-                const task = todos[index];
                 return (
-                  <div className="list-row" key={key} style={style}>
-                    {task.id}, {task.title}, {task.description}
-                    <Button onClick={() => removeTask(task.id, index)}>Delete</Button>
-                  </div>
+                  <Todo
+                    index={index}
+                    key={key}
+                    removeTask={removeTask}
+                    selectTaskIndex={selectTaskIndex}
+                    style={style}
+                    task={todos[index]}
+                  />
                 );
               }}
               width={width}
@@ -59,6 +91,7 @@ TodoList.propTypes = {
   loadTasks: PropTypes.func,
   addTask: PropTypes.func,
   removeTask: PropTypes.func,
+  selectTaskIndex: PropTypes.func,
   todos: PropTypes.array.isRequired
 };
 
