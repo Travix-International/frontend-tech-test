ebb9056e6208fd5215bf5f4183066d51cc0591f6
Fellipe Chagas <chagas.0@gmail.com>
2017-09-30 18:46:21 -0300

    Add edition mode to task item

--- tasks/src/components/molecules/TaskItem/index.js
+++ tasks/src/components/molecules/TaskItem/index.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import TaskTitle from '../../atoms/TaskTitle/index'
-import TaskDescription from '../../atoms/TaskDescription/index'
-import TaskDeleteButton from '../../atoms/TaskDeleteButton/index'
+import TaskContent from '../TaskContent/index'
+import TaskForm from '../TaskForm/index'
 
-const TaskItem = ({ id, title, description, deleteTask }) => {
-  return (
-    <li key={id}>
-      <TaskTitle>{title}</TaskTitle>
-      <TaskDescription>{description}</TaskDescription>
-      <TaskDeleteButton onClick={deleteTask} />
-    </li>
-  )
+class TaskItem extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      editionMode: false
+    }
+  }
+
+  enableEditionMode = () => {
+    this.setState({ editionMode: true })
+  }
+
+  disableEditionMode = () => {
+    this.setState({ editionMode: false })
+  }
+
+  updateTask = (payload) => {
+    this.disableEditionMode()
+    this.props.updateTask(payload)
+  }
+
+  render() {
+    if (this.state.editionMode) {
+      return (
+        <TaskForm
+          {...this.props}
+          updateTask={this.updateTask}
+        />
+      )
+    }
+    return (
+      <TaskContent
+        {...this.props}
+        enableEdit={this.enableEditionMode}
+      />
+    )
+  }
 }
 
 TaskItem.propTypes = {
--- tasks/src/components/molecules/TaskItem/index.test.js
+++ tasks/src/components/molecules/TaskItem/index.test.js
@@ -2,12 +2,8 @@ import React from 'react'
 import ShallowRenderer from 'react-test-renderer/shallow'
 import TaskItem from './index'
 
-import TaskTitle from '../../atoms/TaskTitle/index'
-import TaskDescription from '../../atoms/TaskDescription/index'
-import TaskDeleteButton from '../../atoms/TaskDeleteButton/index'
-
 describe('TaskItem', () => {
-  it('should render correctly', () => {
+  it('should render a task item', () => {
     const renderer = new ShallowRenderer()
     const task = {
       id: 1,
@@ -28,11 +24,93 @@ describe('TaskItem', () => {
     )
     const result = renderer.getRenderOutput()
 
-    expect(result.type).toEqual('li')
-    expect(result.props.children).toEqual([
-      <TaskTitle>A title</TaskTitle>,
-      <TaskDescription>A description</TaskDescription>,
-      <TaskDeleteButton onClick={deleteTask}/>
-    ])
+    expect(result).toMatchSnapshot()
+  })
+
+  describe('when edition mode is enabled', () => {
+    it('should render a task form', () => {
+      const renderer = new ShallowRenderer()
+      const task = {
+        id: 1,
+        title: 'A title',
+        description: 'A description'
+      }
+      const updateTask = jest.fn()
+      const deleteTask = jest.fn()
+
+      renderer.render(
+        <TaskItem
+          id={task.id}
+          title={task.title}
+          description={task.description}
+          updateTask={updateTask}
+          deleteTask={deleteTask}
+        />
+      )
+      renderer._instance.enableEditionMode()
+      const result = renderer.getRenderOutput()
+
+      expect(result).toMatchSnapshot()
+    })
+  })
+
+  describe('when edition mode is disabled', () => {
+    it('should render a task content', () => {
+      const renderer = new ShallowRenderer()
+      const task = {
+        id: 1,
+        title: 'A title',
+        description: 'A description'
+      }
+      const updateTask = jest.fn()
+      const deleteTask = jest.fn()
+
+      renderer.render(
+        <TaskItem
+          id={task.id}
+          title={task.title}
+          description={task.description}
+          updateTask={updateTask}
+          deleteTask={deleteTask}
+        />
+      )
+      renderer._instance.disableEditionMode()
+      const result = renderer.getRenderOutput()
+
+      expect(result).toMatchSnapshot()
+    })
+  })
+
+  describe('when update is called', () => {
+    it('should disable edition mode and render the task content', () => {
+      const renderer = new ShallowRenderer()
+      const payload = {
+        id: 1,
+        title: 'An updated title',
+        description: 'A updated description'
+      }
+      const task = {
+        id: 1,
+        title: 'A title',
+        description: 'A description'
+      }
+      const updateTask = jest.fn()
+      const deleteTask = jest.fn()
+
+      renderer.render(
+        <TaskItem
+          id={task.id}
+          title={task.title}
+          description={task.description}
+          updateTask={updateTask}
+          deleteTask={deleteTask}
+        />
+      )
+      renderer._instance.updateTask(payload)
+      const result = renderer.getRenderOutput()
+
+      expect(updateTask).toBeCalledWith(payload)
+      expect(result).toMatchSnapshot()
+    })
   })
 })
