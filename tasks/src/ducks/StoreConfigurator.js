b6314ab33d40577ddee5e9922f1fef627478af7e
Fellipe Chagas <chagas.0@gmail.com>
2017-10-01 17:59:28 -0300

    Apply redux-thunk middleware to redux

--- tasks/src/ducks/StoreConfigurator.js
+++ tasks/src/ducks/StoreConfigurator.js
@@ -1,16 +1,20 @@
 import tasks from './modules/tasks'
+import thunk from 'redux-thunk'
 
 export default class StoreConfigurator {
-  constructor(createStore, combineReducers) {
+  constructor(createStore, combineReducers, applyMiddleware, thunk) {
     this.createStore = createStore || require('redux').createStore
     this.combineReducers = combineReducers || require('redux').combineReducers
+    this.applyMiddleware = applyMiddleware || require('redux').applyMiddleware
+    this.thunk = thunk || require('redux-thunk').default
   }
 
   configure(initialState) {
     const rootReducer = this._buildReducer()
     return this.createStore(
       rootReducer,
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      initialState,
+      this.applyMiddleware(this.thunk)
     )
   }
 
--- tasks/src/ducks/StoreConfigurator.test.js
+++ tasks/src/ducks/StoreConfigurator.test.js
@@ -6,14 +6,19 @@ describe('StoreConfigurator', () => {
   it('should configure redux store', () => {
     const initialState = undefined
     const createStore = jest.fn()
+    const thunk = jest.fn()
     const combineReducers = jest.fn().mockReturnValue('combinedReducer')
+    const applyMiddleware = jest.fn().mockReturnValue('applyMiddleware')
     const configurator = new StoreConfigurator(
-      createStore, combineReducers
+      createStore, combineReducers, applyMiddleware, thunk
     )
 
     configurator.configure(initialState)
 
-    expect(createStore).toBeCalledWith('combinedReducer', initialState)
+    expect(createStore).toBeCalledWith(
+      'combinedReducer', initialState, 'applyMiddleware'
+    )
+    expect(applyMiddleware).toBeCalledWith(thunk)
     expect(combineReducers).toBeCalledWith({
       tasks
     })
