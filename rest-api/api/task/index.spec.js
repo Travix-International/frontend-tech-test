cd0c87bd6312c03937eccb5013dbbc1cb86c1efe
Kaan Özcan <knzcnn@gmail.com>
2017-10-09 04:20:20 +0300

    unit test

--- rest-api/api/task/index.spec.js
+++ rest-api/api/task/index.spec.js
@@ -1,6 +1,12 @@
 import { assert, expect } from "chai";
 import httpMocks from "node-mocks-http";
-import task from "./";
+import proxyquire from "proxyquire";
+
+const task = proxyquire.noCallThru().load("./", {
+  "../../schemas/Todo": {
+    getAll: () => new Promise((resolve) => resolve([]))
+  }
+}).default;
 
 function buildResponse () {
   return httpMocks.createResponse({ eventEmitter: require("events").EventEmitter });
@@ -10,10 +16,10 @@ describe("Route: GET /", () => {
   it("Should return response status 200", (done) => {
     const response = buildResponse();
     const request  = httpMocks.createRequest({
-      method: "POST",
+      method: "GET",
       url: "/"
     });
-
+    console.log(request, response);
     response.on("end", function () {
       expect(response.statusCode).to.be.equal(200);
 
