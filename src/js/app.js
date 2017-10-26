11ae3c9dffeadce2465b6c13216298661646c6b4
Carlos Rincones <carlos.rincones@jobsity.com>
2017-10-25 21:59:37 -0500

    feat: add index.html boilerplate files
    Add index.html entry files
    
    Add main styles file
    
    Add main react component file

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import '../styles/main.scss';
+
+const App = () => (
+  <div>Todo App</div>
+);
+
+render(<App />, document.getElementById('app'));
