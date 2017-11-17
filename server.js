7e39e4b469fb3ef819515ea35d876fbfd7cbfa46
Elton Mesquita <callmeelton@gmail.com>
2017-11-17 14:29:51 -0300

    Fixed server

--- server.js
+++ server.js
@@ -2,10 +2,30 @@
 
 const app = require('express')();
 const tasksContainer = require('./tasks.json');
+const fs = require('fs');
+
+/**
+ * Gets the current (in-menory) task list
+ * and writes to file
+ */
+function writeToFile() {
+  const stringified = JSON.stringify(tasksContainer, null, 2);
+
+  return new Promise((resolve, reject) => {
+    fs.writeFile(
+      './tasks.json',
+      stringified,
+      (error) => {
+        if(error) reject(error);
+
+        resolve();
+      });
+  })
+}
 
 /**
  * GET /tasks
- * 
+ *
  * Return the list of tasks with status code 200.
  */
 app.get('/tasks', (req, res) => {
@@ -14,11 +34,11 @@ app.get('/tasks', (req, res) => {
 
 /**
  * Get /task/:id
- * 
+ *
  * id: Number
- * 
+ *
  * Return the task for the given id.
- * 
+ *
  * If found return status code 200 and the resource.
  * If not found return status code 404.
  * If id is not valid number return status code 400.
@@ -27,7 +47,7 @@ app.get('/task/:id', (req, res) => {
   const id = parseInt(req.params.id, 10);
 
   if (!Number.isNaN(id)) {
-    const task = tasks.Container.find((item) => item.id === id);
+    const task = tasksContainer.tasks.find((item) => item.id === id);
 
     if (task !== null) {
       return res.status(200).json({
@@ -47,33 +67,42 @@ app.get('/task/:id', (req, res) => {
 
 /**
  * PUT /task/update/:id/:title/:description
- * 
+ *
  * id: Number
  * title: string
  * description: string
- * 
+ *
  * Update the task with the given id.
  * If the task is found and update as well, return a status code 204.
  * If the task is not found, return a status code 404.
  * If the provided id is not a valid number return a status code 400.
  */
 app.put('/task/update/:id/:title/:description', (req, res) => {
-  const id = parseInt(req.params.id, 10);
+  let { id, title, description } = req.params
+  id = parseInt(id, 10);
 
   if (!Number.isNaN(id)) {
-    const task = tasksContainer.tasks.find(item => item.id === id);
+    const taskIndex = tasksContainer.tasks.findIndex(item => item.id === id);
 
-    if (task !== null) {
-      task.title = req.params.title;
-      task.description = req.params.description;
-      return res.status(204);
+    if (taskIndex !== null) {
+      tasksContainer.tasks[taskIndex] = { id, title, description };
+
+      writeToFile()
+        .then(() => {
+          res.status(204).json();
+        })
+        .catch(error => {
+          res.status(501).json({
+            message: error,
+          })
+        })
     } else {
-      return res.status(404).json({
+      res.status(404).json({
         message: 'Not found',
       });
     }
   } else {
-    return res.status(400).json({
+    res.status(400).json({
       message: 'Bad request',
     });
   }
@@ -81,10 +110,10 @@ app.put('/task/update/:id/:title/:description', (req, res) => {
 
 /**
  * POST /task/create/:title/:description
- * 
+ *
  * title: string
  * description: string
- * 
+ *
  * Add a new task to the array tasksContainer.tasks with the given title and description.
  * Return status code 201.
  */
@@ -97,16 +126,24 @@ app.post('/task/create/:title/:description', (req, res) => {
 
   tasksContainer.tasks.push(task);
 
-  return res.status(201).json({
-    message: 'Resource created',
-  });
+  writeToFile()
+    .then(() => {
+      res.status(201).json({
+        message: 'Resource created',
+      });
+    })
+    .catch(error => {
+      res.status(501).json({
+        message: error,
+      });
+    })
 });
 
 /**
  * DELETE /task/delete/:id
- * 
+ *
  * id: Number
- * 
+ *
  * Delete the task linked to the  given id.
  * If the task is found and deleted as well, return a status code 204.
  * If the task is not found, return a status code 404.
@@ -116,16 +153,24 @@ app.delete('/task/delete/:id', (req, res) => {
   const id = parseInt(req.params.id, 10);
 
   if (!Number.isNaN(id)) {
-    const task = tasksContainer.tasks.find(item => item.id === id);
-  
-    if (task !== null) {
-      const taskIndex = tasksContainer.tasks;
+    const taskIndex = tasksContainer.tasks.findIndex(item => item.id === id);
+
+    if (taskIndex !== null) {
       tasksContainer.tasks.splice(taskIndex, 1);
-      return res.status(200).json({
-        message: 'Updated successfully',
-      });
+
+      writeToFile()
+        .then(() => {
+          res.status(201).json({
+            message: 'Updated successfully',
+          });
+        })
+        .catch(error => {
+          res.status(501).json({
+            message: error,
+          });
+        })
     } else {
-      return es.status(404).json({
+      return res.status(404).json({
         message: 'Not found',
       });
     }
