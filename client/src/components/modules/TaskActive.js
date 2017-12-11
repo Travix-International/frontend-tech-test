e472360e63fe3c97ee1bfed9e7038b6b2a1a328e
Vitor Margis <vitor.margis@gmail.com>
2017-12-11 05:05:52 -0200

    Encodes title and description for saving

--- client/src/components/modules/TaskActive.js
+++ client/src/components/modules/TaskActive.js
@@ -5,6 +5,7 @@ import Textarea from "react-textarea-autosize";
 export const TaskActive = ({ task, onTaskToggle, onTaskDelete, onTaskUpdate }) => {
   const onToggle = () => onTaskToggle(task);
   const onDelete = () => onTaskDelete(task);
+  const onUpdate = event => onTaskUpdate(event, task);
   const onUpdateTitle = event => onTaskUpdate(event, task, 'title');
   const onUpdateDescription = event => onTaskUpdate(event, task, 'description');
   const placeholder = 'Add a task description...';
@@ -13,7 +14,7 @@ export const TaskActive = ({ task, onTaskToggle, onTaskDelete, onTaskUpdate }) =
     <li key={task.id} className={`task-list-item`}>
       <span className="on-task-done" onClick={onToggle}>O</span>
       <span className="on-task-delete" onClick={onDelete}>X</span>
-      <form onSubmit={onTaskUpdate}>
+      <form onSubmit={onUpdate}>
         <input type="text" value={task.title} onChange={onUpdateTitle} onBlur={onUpdateTitle} />
         <Textarea
           value={task.description}
