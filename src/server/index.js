ace04a524fce194207f3008fa2bafd665d74ff0b
ddhp <sevenknee@gmail.com>
2017-12-25 14:13:37 +0800

    trackingTransport

--- src/server/index.js
+++ src/server/index.js
@@ -7,6 +7,12 @@ app.use(tasksMiddleware);
 
 app.listen(9001, () => {
   logger.debug('the server is available on http://localhost:9001/');
+
+  // tracking sample
+  logger.info('LOG_WITH_TRACK_SAMPLE', '', {
+    isTrack: true,
+  });
 });
+
 
 export default app;
--- src/server/log.js
+++ src/server/log.js
@@ -1,10 +1,12 @@
 import { Logger } from 'travix-logger';
 import logLevelTransport from '../logLevelTransport';
+import trackingTransport from '../trackingTransport';
 
 const logger = new Logger({
   transports: [
     // support same options as configureConsoleTransport
     logLevelTransport(),
+    trackingTransport(),
   ],
 });
 
