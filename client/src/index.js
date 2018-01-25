0f81044284f4901fdb1c38e45e0c1b30a63b30ae
Alaa Haddad <alaahd@gmail.com>
2018-01-25 10:52:19 +0400

    add sass integration

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
-import './index.css';
+import './styles/index.css';
 import App from './containers/App';
 
 ReactDOM.render(
