c5ad41543a5668f1d3c2288c3b01cc15d7cb8f92
andrii <malykhin.andrey@gmail.com>
2018-01-31 00:14:57 +0200

    add loading spinner to dashboard page

--- components/HomePage/index.jsx
+++ components/HomePage/index.jsx
@@ -2,11 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { observe } from 'frint-react';
 import { of } from 'rxjs/observable/of';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { map, merge, scan } from 'rxjs/operators';
+import { LoadingOverlay } from 'travix-ui-kit';
 import Form from '../Form';
 import List from '../List';
 import { addTodo } from '../../actions/todos';
 
 function HomePage(props) {
+  if (props.loading) {
+    return (<LoadingOverlay
+      loading
+      spinner
+    >
+      <h2 key="dashboardTitle">Dashboard</h2>
+      <Form
+        action={props.addTodo}
+        actionBtnTitle="Add"
+        formTitle="Create a new Todo item"
+        key="dashboardForm"
+      />
+    </LoadingOverlay>);
+  }
+
   return ([
     <h2 key="dashboardTitle">Dashboard</h2>,
     <Form
@@ -21,12 +39,33 @@ function HomePage(props) {
 
 HomePage.propTypes = {
   addTodo: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 export default observe((app) => {
   const store = app.get('store');
+  const loading$ = new BehaviorSubject(false).pipe(
+    map(loading => ({ loading })),
+  );
 
-  return of({
+  const state$ = store.getState$();
+
+  const stateProps$ = state$
+    .pipe(
+      map((state) => {
+        loading$.next(state.server);
+      }),
+    );
+
+  const actions$ = of({
     addTodo: (...args) => store.dispatch(addTodo(...args)),
   });
+
+  return stateProps$.pipe(
+    merge(actions$, loading$),
+    scan((props, emitted) => ({
+      ...props,
+      ...emitted,
+    })),
+  );
 })(HomePage);
