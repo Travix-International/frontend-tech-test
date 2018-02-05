7e020add6e91aebf4be9f3db96dfd88a34fe52b0
Fernando Fajardo <fernando.fajardo@zagalabs.com>
2018-02-05 18:34:04 -0500

    add loading component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,10 +4,12 @@ import React from 'react';
 
 import TodoBoxContainer from '../../containers/TodoBoxContainer';
 import TodoForm from '../../components/TodoForm';
+import Loading from '../../components/Loading';
 
 function Layout(){
   return (
     <div class="layout">
+      <Loading />
       <TodoForm />
       <div class="layout-container">
       <TodoBoxContainer />
