c0ad893f46c7aa669f9615c87b058e63049ef961
Umberto Di Lorenzo <dilorenzo@gmail.com>
2018-02-07 17:29:46 +0100

    enabled CORS in server.js

--- server.js
+++ server.js
@@ -3,6 +3,14 @@
 const app = require('express')();
 const tasksContainer = require('./tasks.json');
 
+// enable CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "DELETE, GET, POST, PUT, OPTIONS");
+  next();
+});
+
 /**
  * GET /tasks
  * 
