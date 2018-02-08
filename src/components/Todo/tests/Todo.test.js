b69942d5fc5993d7cb20dbe29f596af61d4db014
Umberto Di Lorenzo <umberto.dilorenzo@buzzoole.com>
2018-02-08 01:26:20 +0100

    added more Todo component unit testing

--- src/components/Todo/tests/Todo.test.js
+++ src/components/Todo/tests/Todo.test.js
@@ -6,18 +6,30 @@ import { shallow } from 'enzyme';
 import Todo from '../Todo';
 import UpdateTodo from '../../../containers/UpdateTodo/UpdateTodo';
 
+import mock from '../../../mocks/tasks';
+
+const todo = mock.tasks[0];
+
 const todoWrapper = shallow(
   <Todo
-    completed
-    description={"Gotta create a Todo app"}
+    completed={todo.completed}
+    description={todo.description}
     editing
-    id={3}
-    key={3}
+    id={todo.id}
+    key={todo.id}
     onClick={() => { }}
-    title={"Create a Todo app"}
+    title={todo.title}
   />
 );
 
 it('Todo will display UpdateTodo form when editing prop is true', () => {
   expect(todoWrapper.containsMatchingElement(<UpdateTodo />)).toEqual(true);
+});
+
+it('Todo will display title of object passed in props', () => {
+  expect(todoWrapper.find('.todoTitle').text()).toEqual(todo.title);
+});
+
+it('Todo will display description of object passed in props', () => {
+  expect(todoWrapper.find('.todoDescription').text()).toEqual(todo.description);
 });
