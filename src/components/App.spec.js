081d61d4c8c081cbb76365c8e5916b97c60eb073
llinares <lean8086@gmail.com>
2018-06-24 15:56:54 -0300

    Add base tests

---
+++ src/components/App.spec.js
@@ -0,0 +1,21 @@
+const React = require('react');
+const { shallow } = require('enzyme');
+const App = require('./App');
+
+describe('<App/>', () => {
+  it('render the application', () => {
+    shallow(<App />);
+  });
+
+  it('should render the basic layout elements', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('header[role="banner"]')).toHaveLength(1);
+    expect(wrapper.find('main')).toHaveLength(1);
+  });
+
+  it('should render the logo', () => {
+    const wrapper = shallow(<App />);
+    const logo = <h1 className="banner__logo">Travix</h1>;
+    expect(wrapper.contains(logo)).toEqual(true);
+  });
+});
