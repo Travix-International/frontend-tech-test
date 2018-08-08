9007579216a7abd64f1533e798ae55f97311fe48
Александр Брюзгин < >
2018-08-08 23:18:45 +0300

    ejected project, added react toolbox

--- todo/src/components/Header/index.jsx
+++ todo/src/components/Header/index.jsx
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
-import Button from './../ui/Button';
-import './Header.css';
+import { Button } from 'react-toolbox/lib/button';
+import styles from './Header.css';
 
 class Header extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
-      <header className="header">
-        <Button text="Main" href="" />
-        <Button text="New" href="/new" />
+      <header className={styles.header}>
+        <Button icon="arrow_back" href="" mini />
+        <Button label="New" icon="add" href="/new" primary raised/>
       </header>
     )
   }
--- todo/src/components/Main/index.jsx
+++ todo/src/components/Main/index.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getItems } from './../../store/actions/actions';
-import Button from './../ui/Button';
-import List from '../ui/List';
+import { Card, CardTitle } from 'react-toolbox/lib/card';
+import Checkbox from 'react-toolbox/lib/checkbox';
 import './Main.css';
 
 class Main extends Component {
@@ -34,21 +34,14 @@ class Main extends Component {
   onChangeTodoState(evt) {}
 
   renderList() {
-    const items = this.state.items.map((item) => {
-      const spanClass = item.done ? "item_name item_name--done" : "item_name";
-      return (
-        <div className="item_block">
-          <div className="item_left">
-            <input type="checkbox" className="item_checkbox" onChange={this.onChangeTodoState} checked={item.done}/>
-            <span className={spanClass}>{item.name}</span>
-          </div>
-          <div className="item_right">
-            <Button text="x" onClick={this.deleteItem} />
-          </div>
-        </div>
-      );
-    });
-    return <List items={items} className="list--todos"/>
+    return this.state.items.map((item, index) => (
+        <Card style={{width: '25%'}} key={`${index}--${item.name}`}>
+          <CardTitle title={item.name} subtitle={item.description}>
+            <Checkbox onChange={this.onChangeTodoState} checked={item.done}/>
+          </CardTitle>
+        </Card>
+      )
+    );
   }
   render() {
     return (
