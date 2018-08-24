9e41ad9537b01fd86cc4f9a37490897a89795c2f
Pavel Ivanov <pablospaniard@gmail.com>
2018-08-24 11:14:42 +0200

    server updated

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict'
 
-const app = require('express')()
+const express = require('express')
+const app = express()
 var cors = require('cors')
 const tasksContainer = require('./tasks.json')
 
