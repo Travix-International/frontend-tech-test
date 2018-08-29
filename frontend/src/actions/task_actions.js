aec88ecb1c23ea14e7ced2e4a850640bfd9694f5
Shaurya Sinha <shaurya157@gmail.com>
2018-08-29 15:11:54 +0800

    Added additional actions for success and error

--- frontend/src/actions/task_actions.js
+++ frontend/src/actions/task_actions.js
@@ -1,5 +1,9 @@
 import {CREATE_TASK} from 'constants/ActionTypes'
 
+const logger = new Logger({
+  transports: [configureConsoleTransport()],
+})
+
 export const createTask = (task) => ({
   type: CREATE_TASK,
   task
