94d11cb69bd3fc46f5b7824c3fa8a6c0d950b0ff
herat130 <herat130@gmail.com>
2018-10-03 15:47:02 +0530

    to avoid 304 status code
    to avoid 304 status code

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const app = require('express')();
 app.use(function (req, res, next) {
+  req.headers['if-none-match'] = 'no-match-for-this';
   res.header("Access-Control-Allow-Origin", "*");
   // res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
