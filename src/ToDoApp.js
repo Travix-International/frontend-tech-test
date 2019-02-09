67d16af0254f8ef469861907b086445dde025a64
Mahesh Pandekar <pmahesh6789@gmail.com>
2019-02-09 12:29:17 +0100

    Initial react setup.

---
+++ src/ToDoApp.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './ToDoApp.css';
+
+class ToDoApp extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Demo
+        </header>
+      </div>
+    );
+  }
+}
+
+export default ToDoApp;
