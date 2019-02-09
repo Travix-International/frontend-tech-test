a2218c4ad5275656ec93f2e07412beb3db863ac2
Mahesh Pandekar <pmahesh6789@gmail.com>
2019-02-09 14:18:16 +0100

    Updated setup & added bundaling task using webpack.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import ToDoApp from './ToDoApp';
 import * as serviceWorker from './serviceWorker';
+import FormContainer from "./components/container/FormContainer.jsx";
 
-ReactDOM.render(<ToDoApp />, document.getElementById('root'));
+ReactDOM.render(<FormContainer />, document.getElementById('create-article-form'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
