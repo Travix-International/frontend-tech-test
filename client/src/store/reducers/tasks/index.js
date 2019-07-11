34cce6586ca87960196a885e29e74adcc3999c98
Ivan Burnaev <ivan.biirus@gmail.com>
2019-07-11 22:20:48 +0300

    refactor(tasks-reducer): move action handlers to separate files to increase testability

--- client/src/store/reducers/tasks/index.js
+++ client/src/store/reducers/tasks/index.js
@@ -1,43 +1,21 @@
 import { handleActions } from "redux-actions";
-import { getSuccessType, getPendingType } from "redux-async-epic";
+import { getSuccessType } from "redux-async-epic";
 import Immutable from "seamless-immutable";
 import { types } from "./actions";
+import lib from "./lib";
 
-const initialState = Immutable({
+export const initialState = Immutable({
   tasks: {},
   filter: "undone",
-  uiState: "idle",
 });
-
-const mapTasksByID = tasks => {
-  return tasks.reduce((acc, task) => {
-    task.done = task.done || false;
-
-    acc[task.id] = task;
-    return acc;
-  }, {});
-};
 
 export default handleActions(
   {
-    [getPendingType(types.fetchList)]: (state, { payload }) => {
-      return state.set("uiState", payload ? "pending" : "success");
-    },
-    [getSuccessType(types.fetchList)]: (state, action) => {
-      return state.set("tasks", mapTasksByID(action.payload));
-    },
-    [getSuccessType(types.change)]: (state, { payload }) => {
-      return state.setIn(["tasks", payload.id], payload);
-    },
-    [getSuccessType(types.remove)]: (state, { payload }) => {
-      return state.update("tasks", tasks => tasks.without(payload));
-    },
-    [getSuccessType(types.create)]: (state, { payload }) => {
-      return state.update("tasks", tasks => tasks.set(payload.id, payload));
-    },
-    [types.changeFilter]: (state, { payload }) => {
-      return state.set("filter", payload);
-    },
+    [getSuccessType(types.fetchList)]: lib.fulfillList,
+    [getSuccessType(types.change)]: lib.changeTask,
+    [getSuccessType(types.remove)]: lib.removeTask,
+    [getSuccessType(types.create)]: lib.createTask,
+    [types.changeFilter]: lib.changeFilter,
   },
   initialState
 );
---
+++ client/src/store/reducers/tasks/lib.js
@@ -0,0 +1,36 @@
+export const mapTasksByID = tasks => {
+  return tasks.reduce((acc, task) => {
+    task.done = task.done || false;
+
+    acc[task.id] = task;
+    return acc;
+  }, {});
+};
+
+export const fulfillList = (state, action) => {
+  return state.set("tasks", mapTasksByID(action.payload));
+};
+
+export const changeTask = (state, { payload }) => {
+  return state.setIn(["tasks", payload.id], payload);
+};
+
+export const removeTask = (state, { payload }) => {
+  return state.update("tasks", tasks => tasks.without(payload));
+};
+
+export const createTask = (state, { payload }) => {
+  return state.update("tasks", tasks => tasks.set(payload.id, payload));
+};
+
+export const changeFilter = (state, { payload }) => {
+  return state.set("filter", payload);
+};
+
+export default {
+  fulfillList,
+  changeTask,
+  removeTask,
+  createTask,
+  changeFilter,
+};
