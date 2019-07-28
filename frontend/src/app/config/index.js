25b3ad9aa72b5f6edd6cc22c9c42c53cb8ee7e87
Siamak A.Motlagh <siamak.aghaeipour@gmail.com>
2019-07-28 21:06:42 +0430

    Add config directory and file
    * This is created due to handle default configuration of project
    * API confugurations are read from env file
    * Default routes object is created (Object is empty for now)

---
+++ frontend/src/app/config/index.js
@@ -0,0 +1,9 @@
+// Extract api config from env file
+export const api = {
+  domain: process.env.REACT_APP_API_HOST,
+  port: process.env.REACT_APP_API_PORT,
+  version: process.env.REACT_APP_API_VERSION
+};
+
+// Default routes
+export const routes = {};
