5d2680ff6ff8fac774f7d17f8fa82a7633bd8f31
Siamak A.Motlagh <siamak.aghaeipour@gmail.com>
2019-07-28 21:59:47 +0430

    List component refactored

--- frontend/src/app/components/List/List.js
+++ frontend/src/app/components/List/List.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Wrapper from './assets/style';
+import ListItem from '../ListItem';
 
 const List = () => {
-  return <div>list</div>;
+  return (
+    <Wrapper>
+      <ListItem
+        key={1}
+        id={1}
+        title="Go home"
+        description="Go home soon you need to wait for Atefeh. Go home soon you need to wait for. Go home soon you need."
+      />
+      <ListItem key={2} id={2} title="Buy pizza" description="Your turn to cook" />
+    </Wrapper>
+  );
 };
 
 export default List;
