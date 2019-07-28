3de50926b63c5638b88789c4723708b67e134393
Siamak A.Motlagh <siamak.aghaeipour@gmail.com>
2019-07-28 23:24:11 +0430

    Add dynamic routing
    * Routes need to define in config file

--- frontend/src/app/Routes.js
+++ frontend/src/app/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import List from './components/List';
+import { routes } from './config';
 
 /*
  * Define main routes of the app here.
@@ -9,12 +9,14 @@ const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={List} exact />
-        <Route path="/tasks" component={List} />
-        <Route path="/tasks/:id" component={List} />
-        <Route path="/tasks/:id/add" component={List} />
-        <Route path="/tasks/:id/edit" component={List} />
-        <Route path="/tasks/:id/delete" component={List} />
+        {Object.keys(routes).map(route => (
+          <Route
+            path={routes[route].path}
+            render={routes[route].component}
+            exact={routes[route].exact}
+            key={routes[route].path}
+          />
+        ))}
       </Switch>
     </Router>
   );
--- frontend/src/app/config/index.js
+++ frontend/src/app/config/index.js
@@ -1,9 +1,42 @@
+import List from '../components/List';
+
+// Default routes
+export const routes = {
+  tasks: {
+    path: '/',
+    component: List,
+    excat: true,
+    description: 'Task List'
+  },
+  viewTask: {
+    path: '/task/:id',
+    component: List,
+    excat: false,
+    description: 'View Task'
+  },
+  newTask: {
+    path: '/tasks/add',
+    component: List,
+    excat: false,
+    description: 'New Task'
+  },
+  editTask: {
+    path: '/tasks/:id/edit',
+    component: List,
+    excat: false,
+    description: 'Edit Task'
+  },
+  deleteTask: {
+    path: '/tasks/:id/delete',
+    component: List,
+    excat: false,
+    description: 'Delete Task'
+  }
+};
+
 // Extract api config from env file
 export const api = {
   domain: process.env.REACT_APP_API_HOST,
   port: process.env.REACT_APP_API_PORT,
   version: process.env.REACT_APP_API_VERSION
 };
-
-// Default routes
-export const routes = {};
