55082a702055bd27c5cc2a2175317e297ce6ca9e
Siamak A.Motlagh <siamak.aghaeipour@gmail.com>
2019-07-31 15:59:20 +0430

    Input and textarea added

--- frontend/src/app/components/TaskAdd/TaskAdd.js
+++ frontend/src/app/components/TaskAdd/TaskAdd.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Section from '../Section';
+import Input from '../Form';
 
 const TaskAdd = () => {
-  return <Section>add</Section>;
+  const [title, setTitle] = useState({ value: '', error: '' });
+  const [description, setDescription] = useState({ value: '', error: '' });
+
+  useEffect(() => {
+    if (title.value.trim() === '') {
+      setTitle({ value: title.value, error: 'Title cannot be empty.' });
+    } else {
+      setTitle({ value: title.value, error: '' });
+    }
+  }, [title.value]);
+
+  console.log(description);
+
+  return (
+    <Section>
+      <Input
+        onChange={value => setTitle({ value, error: '' })}
+        error={title.error}
+        id="task-title"
+        label="Title"
+        placeholder="Write task title here"
+      />
+      <Input
+        type="textarea"
+        onChange={value => setDescription({ value, error: '' })}
+        id="task-description"
+        label="Description"
+        placeholder="Write task description here"
+      />
+    </Section>
+  );
 };
+
 export default TaskAdd;
